5bbf4b3a46704ded7ab800b6553f7ed38fdd89f3
Wako Oshiro <e165711@ie.u-ryukyu.ac.jp>
2016-11-24 19:08:49 +0900

    No.00

--- src/main/jp.ac.uryukyu.ie.e165711/Enemy.java
+++ src/main/jp.ac.uryukyu.ie.e165711/Enemy.java
@@ -45,12 +45,14 @@ public class Enemy {
      * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
      * @param hero 攻撃対象
      */
+
     public void attack(Hero hero){
+        int damage = 0;
         if(dead == false) {
-            int damage = (int) (Math.random() * attack);
+            damage = (int) (Math.random() * attack);
             System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
-            hero.wounded(damage);
         }
+            hero.wounded(damage);
     }
 
     /**
