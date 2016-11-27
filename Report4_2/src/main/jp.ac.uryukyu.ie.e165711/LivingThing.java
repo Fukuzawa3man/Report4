45fd4740e6c2896168ddcabf68069fac71a2376d
Wako Oshiro <e165711@ie.u-ryukyu.ac.jp>
2016-11-27 18:13:22 +0900

    No.02_3

--- Report4_2/src/main/jp.ac.uryukyu.ie.e165711/LivingThing.java
+++ Report4_2/src/main/jp.ac.uryukyu.ie.e165711/LivingThing.java
@@ -10,44 +10,62 @@ package jp.ac.uryukyu.ie.e165711;
  */
 
 public class LivingThing {
-    String name;
-    int hitPoint;
-    int attack;
-    boolean dead;
+    private String name;
+    private int hitPoint;
+    private int attack;
+    private boolean dead;
 
     /**
      * コンストラクタ。名前、最大HP、攻撃力を指定する。
-     * @param name 勇者orモンスター名
+     *
+     * @param name      勇者orモンスター名
      * @param maximumHP 勇者orモンスターのHP
-     * @param attack 勇者orモンスターの攻撃力
+     * @param attack    勇者orモンスターの攻撃力
      */
-    public LivingThing (String name, int maximumHP, int attack) {
+    public LivingThing(String name, int maximumHP, int attack) {
         this.name = name;
         hitPoint = maximumHP;
         this.attack = attack;
         dead = false;
-        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
+        if (name == "ヨシヒコ") {
+            System.out.printf("勇者%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
+        } else {
+            System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
+        }
+
     }
+
+
 
     /**
      * getterメソッドと同等。生死をboolean表現しているためメソッド名をisDead()とした。
+     *
      * @return boolean
      */
-    public boolean isDead() {
-        return dead;
+    public boolean getIsDead() {
+        return this.dead;
     }
 
-    public String getName(){
-        return name;
+    public void setIsDead(boolean dead) {
+        this.dead = dead;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getHitPoint(){
+        return this.hitPoint;
     }
 
     /**
      * Heroへ攻撃するメソッド。
      * attackに応じて乱数でダメージを算出し、opponent.wounded()によりダメージ処理を実行。
+     *
      * @param opponent 攻撃対象
      */
-    public void attack(LivingThing opponent){
-        if(dead == false) {
+    public void attack(LivingThing opponent) {
+        if (dead == false) {
             int damage = (int) (Math.random() * attack);
             System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, opponent.getName(), damage);
             opponent.wounded(damage);
@@ -57,15 +75,16 @@ public class LivingThing {
     /**
      * 自身へ攻撃されたときのダメージ処理をするメソッド。
      * 指定されたダメージを hitPoint から引き、死亡判定を行う。
+     *
      * @param damage 受けたダメージ
      */
-    public void wounded(int damage){
+    public void wounded(int damage) {
         hitPoint -= damage;
-        if( hitPoint < 0 ) {
+        if (hitPoint < 0) {
             dead = true;
             if (name == "ヨシヒコ") {
                 System.out.printf("勇者%sは道半ばで力尽きてしまった。\n", name);
-            }else{
+            } else {
                 System.out.printf("モンスター%sは倒れた。\n", name);
 
             }
--- Report4_2/src/test/jp.ac.uryukyu.ie.e165711/EnemyTest.java
+++ Report4_2/src/test/jp.ac.uryukyu.ie.e165711/EnemyTest.java
@@ -7,7 +7,6 @@ import static org.junit.Assert.*;
 /**
  * Created by e165711 on 2016/11/26.
  */
-
 public class EnemyTest {
     /**
      * 「死んでる状態では攻撃できない」ことを確認。
@@ -18,11 +17,11 @@ public class EnemyTest {
         int heroHP = 10;
         Hero hero = new Hero("テスト勇者", heroHP, 5);
         Enemy enemy = new Enemy("テストスライム", 6, 3);
-        enemy.dead = true;
+        enemy.setIsDead(true);
         for(int i=0; i<10; i++) {
             enemy.attack(hero); //乱数で0ダメージとなることもあるため、複数回実行してみる。
         }
-        assertEquals(heroHP, hero.hitPoint);
+        assertEquals(heroHP, hero.getHitPoint());
     }
 
 }
